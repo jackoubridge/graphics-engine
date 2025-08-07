cb3209ba1384c28601ee985652e742560125ec23
jackoubridge <jaoubridge@gmail.com>
2025-08-08 00:51:32 +0100

    Basic glfw3 window

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
-#include <iostream>
 #include "main.h"
 
-int main() {
+int main()
+{
+    GLFWwindow* window;
 
-    std::cout << "Hello, World!" << std::endl;
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    window = glfwCreateWindow(1280, 720, "Graphics Engine", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    
 
     return 0;
 }
---
+++ src/main.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW3/glfw3.h>
